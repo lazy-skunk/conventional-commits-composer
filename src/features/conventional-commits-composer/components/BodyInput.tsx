b87662b5657376070fa4af25b4629908cc980549
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2025-08-31 14:55:55 +0000

    feat: add body input

---
+++ src/features/conventional-commits-composer/components/BodyInput.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+type Props = {
+  body: string;
+  onChangeBody: (body: string) => void;
+};
+
+export default function BodyInput({ body, onChangeBody }: Props) {
+  return (
+    <div>
+      <label htmlFor="body" className="block font-bold">
+        Body (optional)
+      </label>
+
+      <textarea
+        id="body"
+        value={body}
+        onChange={(event) => onChangeBody(event.target.value)}
+        className="border rounded w-full p-1 bg-background text-foreground"
+        rows={3}
+        placeholder="Additional contextual information about the code changes"
+      />
+    </div>
+  );
+}
