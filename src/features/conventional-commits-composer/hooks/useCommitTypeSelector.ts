510bca1bbaf6029a967973af836b8f9b9c20f82c
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2025-08-31 15:47:56 +0000

    feat: add useCommitTypeSelector

---
+++ src/features/conventional-commits-composer/hooks/useCommitTypeSelector.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import {
+  DEFAULT_OTHER_TYPE,
+  isOtherType,
+  type CommitType,
+  type OtherType,
+} from "../domain";
+
+export function useCommitTypeSelector(commitType: CommitType) {
+  const [lastSelectedOtherType, setLastSelectedOtherType] =
+    useState<OtherType>(DEFAULT_OTHER_TYPE);
+
+  useEffect(() => {
+    if (isOtherType(commitType)) {
+      setLastSelectedOtherType(commitType);
+    }
+  }, [commitType]);
+
+  return { lastSelectedOtherType };
+}
