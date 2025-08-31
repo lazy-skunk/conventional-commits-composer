285c7fead0fd438ebb5758e65bcae1afeec1fb94
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2025-08-31 15:51:53 +0000

    feat: add composeConventionalCommit

---
+++ src/features/conventional-commits-composer/hooks/useConventionalCommitsComposer.ts
@@ -0,0 +1,55 @@
+import { useMemo, useState } from "react";
+import type { BreakingChangeStyle, CommitType } from "../domain";
+import { composeConventionalCommit } from "../domain";
+
+export function useConventionalCommitsComposer() {
+  const [commitType, setCommitType] = useState<CommitType>("feat");
+  const [scope, setScope] = useState("");
+  const [description, setDescription] = useState("");
+  const [body, setBody] = useState("");
+  const [breakingChangeStyle, setBreakingChangeStyle] =
+    useState<BreakingChangeStyle>("none");
+  const [breakingChangeDescription, setBreakingChangeDescription] =
+    useState("");
+  const [footer, setFooter] = useState("");
+
+  const commitPreview = useMemo(() => {
+    return composeConventionalCommit({
+      commitType,
+      scope,
+      description,
+      body,
+      breakingChangeStyle,
+      breakingChangeDescription,
+      footer,
+    });
+  }, [
+    commitType,
+    scope,
+    description,
+    body,
+    breakingChangeStyle,
+    breakingChangeDescription,
+    footer,
+  ]);
+
+  return {
+    commitType,
+    scope,
+    description,
+    body,
+    breakingChangeStyle,
+    breakingChangeDescription,
+    footer,
+
+    setCommitType,
+    setScope,
+    setDescription,
+    setBody,
+    setBreakingChangeStyle,
+    setBreakingChangeDescription,
+    setFooter,
+
+    commitPreview,
+  };
+}
